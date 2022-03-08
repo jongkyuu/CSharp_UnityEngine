a062de0dca4ecf9b689c612024662226c1cb85f4
minerva421 <minerva421.ai@gmail.com>
2022-03-09 02:48:17 +0900

    ch5 투영의 개념

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -31,39 +31,11 @@ public class TestCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 20-1
-        // 특정 방향으로 레이저를 쏨. Raycast는 기본적으로 앞에있는 하나의 물체만 타겟팅 한다
-        // DrawRay를 통해 Raycasting을 쏠 수 있음.
-        //Debug.DrawRay(transform.position + Vector3.up, Vector3.forward * 10, Color.red);  // 20-3 위치에 Vector3.up을 더해 상체에서 레이저가 나가도록 수정
+        // Screen 좌표계 표현
+        // Input.mousePosition : 현재 마우스 좌표를 픽셀 좌표로 뽑아온다. 픽셀 좌표가 Screen 좌표이다. 
+        //Debug.Log(Input.mousePosition);
 
-        // 20-5
-        // 캐릭터가 움직이는 방향에 따라 Raycast도 움직이도록 forward를 로컬 좌표로 인식하고 거기에 해당하는 월드 좌표를 찾으면 된다
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
-
-        // 20-2
-        // Vector3는 위치와 방향을 나타내는데 사용
-        // Physics.Raycast(transform.position, Vector3.forward) 에서 Vector3.forward는 단위벡터인데 이 함수에서는 해당 방향으로 끝까지 뻗어감
-        // Debug.DrawRay는 start 지점부터 start + dir 지점까지 선을 그어주므로 두번째 인자에 방향 뿐만 아니라 크기도 있어야 한다
-
-
-        //if (Physics.Raycast(transform.position + Vector3.up, Vector3.forward, 10))   // 마지막 인자는 maxDistance
-        // 20-4
-        // Raycasting 대상을 알기 위해서 RaycastHit 추가
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position + Vector3.up, Vector3.forward, out hit, 10))
-            if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))  // 20-5 반영해서 forword를 look으로 바꿈
-            {
-                Debug.Log($"Raycast {hit.collider.gameObject.name}!");
-            }
-
-        // 20-6 Raycasting의 일직선상에 있는(관통하는) 모든 물체를 알고싶으면 RaycastAll() 사용
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
-
-        foreach(var hitone in hits)
-        {
-            Debug.Log($"RaycastAll {hitone.collider.gameObject.name}");
-        }
+        // Viewport 
+        Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition));
     }
 }
